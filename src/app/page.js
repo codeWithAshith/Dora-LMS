8a7ea041ce29052d633f0d570d72784c1613c8a1
codewithashith <ashithvl@gmail.com>
2025-02-12 15:35:02 +0530

    shadcn

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <div className="underline">Para - LMS</div>;
+  return <Button>Button</Button>;
 };
 
 export default Home;
